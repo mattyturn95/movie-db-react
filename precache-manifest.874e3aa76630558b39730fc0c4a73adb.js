acd9724e94ff75afff87270472b1fd0d73ef98fa
mattyturn95 <46969824+mattyturn95@users.noreply.github.com>
2020-07-23 12:22:37 +0100

    Updates

---
+++ precache-manifest.874e3aa76630558b39730fc0c4a73adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d153707571479a7b6b992007dc4f586",
+    "url": "/movie-db-react/index.html"
+  },
+  {
+    "revision": "bff532d4163858cccaf3",
+    "url": "/movie-db-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1edcdb6b4962cb7ff736",
+    "url": "/movie-db-react/static/js/2.a9730b93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-db-react/static/js/2.a9730b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff532d4163858cccaf3",
+    "url": "/movie-db-react/static/js/main.3fea7fb1.chunk.js"
+  },
+  {
+    "revision": "c5fbc08d941533143cfe",
+    "url": "/movie-db-react/static/js/runtime-main.2d4c17e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-db-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
